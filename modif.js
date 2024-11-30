f8c87991d7d6162135c6ba15be1b14009030e188
Durak-Git <97435195+Durak-Git@users.noreply.github.com>
2024-12-01 00:23:56 +0100

    Create modif.js

---
+++ modif.js
@@ -0,0 +1,13 @@
+
+import { torgeternity } from "../../systems/torgeternity/module/config.js";
+
+Hooks.once("ready", async function () {
+  torgeternity.cosmTypes.malagwa = 'personnal.malagwa';
+
+  torgeternity.axiomByCosm.malagwa = {
+      magic: 90,
+      social: 90,
+      spirit: 90,
+      tech: 90,
+  };
+})
