24533e5e04fafee79568c4d68b3a13e633b98055
Durak-Git <97435195+Durak-Git@users.noreply.github.com>
2025-09-28 17:32:37 +0200

    Update modif.js

--- modif.js
+++ modif.js
@@ -2,6 +2,8 @@
 import { torgeternity } from "../../systems/torgeternity/module/config.js";
 
 Hooks.once("ready", async function () {
+  console.log(torgeternity.cosmTypes);
+  console.log(torgeternity);
   torgeternity.cosmTypes.malagwa = 'personnal.malagwa';
 
   torgeternity.axiomByCosm.malagwa = {
